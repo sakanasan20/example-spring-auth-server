e662514e304e53436346a0563514f68e855e11e8
Nick <nick.chen@cmp.com.tw>
2023-07-21 11:26:36 +0800

    Initial commit

---
+++ src/main/java/tw/niq/example/ExampleSpringAuthServerApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringAuthServerApplication.class, args);
+	}
+
+}
